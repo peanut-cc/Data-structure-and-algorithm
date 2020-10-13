d8880ebc199ebaa44527046f0cbde0f387f54de3
fan.zhao <fan.zhao@chaitin.com>
2020-10-13 21:10:59 +0800

    add swapairs

---
+++ day02/swapairs/main.go
@@ -0,0 +1,24 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 这种方法其实不难，主要是对于链表这种数据结构操作需要熟悉，这几天要多看几遍加强这种思维训练方式
+func swapPairs(head *ListNode) *ListNode {
+	// 这里声明一个临时ListNode{} 对象，并且它 的Next为Head
+	pre := &ListNode{}
+	pre.Next = head
+	temp := pre
+	// 这里的判断其实就是为了保证每次两两交换的节点是存在的
+	for temp.Next != nil && temp.Next.Next != nil {
+		start := temp.Next
+		end := temp.Next.Next
+		temp.Next = end
+		start.Next = end.Next
+		end.Next = start
+		temp = start
+	}
+	return pre.Next
+}
